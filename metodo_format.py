3dd8216806e462912556c8ff22fcecb3fc5990a1
victor2905 <98538867+victor2905@users.noreply.github.com>
2023-01-27 22:42:52 -0500

    metodo_format
    Creación de proyecto

---
+++ metodo_format.py
@@ -0,0 +1,10 @@
+nombre = "Carlos"
+edad = 22
+#Opción 1:
+print("Hola {} tienes {} años.".format(nombre,edad))
+#Opción 2:
+print("Hola {nombre1} tienes {edad1} años.".format(nombre1="Mayra",edad1=23))
+#Opción 3:
+nombre2="Hugo"
+edad2=24
+print("Hola {0} tienes {1} años.".format(nombre2,edad2))
